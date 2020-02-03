8d246a4bb4cbd1e53746738cd70d1291686b362a
FranciscoSPereira <60528077+FranciscoSPereira@users.noreply.github.com>
2020-02-03 14:02:36 +0000

    Add files via upload

--- Menu.py
+++ Menu.py
@@ -3,54 +3,59 @@ from MyBlast import MyBlast
 from upgma import UPGMA
 from AlignSeq import AlignSeq
 from SubstMatrix import SubstMatrix
-from Bio import Entrez, SeqIO, Phylo
+from Bio import Entrez, SeqIO, Phylo, AlignIO
 from Bio.Blast import NCBIXML, NCBIWWW
 from Bio.Align.Applications import ClustalwCommandline
+from MultipleAlign import MultipleAlign
+import pickle
 import re
 import os
 
 cwd=os.getcwd().strip('src')
 
-class GestorAA:
+class GestorSeq:
    
     def __init__(self):
         self.dic={}
         self.seqs_alfabeto='ACTG'
         self.seqs_tipo='dna'
-        self.name=None
+        self.nome=None
         self.seq=None
-        self.listaIds=[]
+        self.listaids=[]
 
-    def loadFromFile(self,nome):
-        name=nome
-        self.name=name
-        self.listIds()
+    def loadFromFile(self,name):
+        nome=name
+        self.nome=nome
+        self.listaids()
     
-    def createNew(self,nome):
-        name=nome
-        self.name=name
+    def criar(self,name):
+        nome=name
+        self.nome=nome
     
-    def addSeq(self,ident,seq):
-        Id=ident
-        sequen=seq
-        if self.valSeq(seq):
-            self.listaIds.append(Id)
+    def adicionar(self,identificador,seq):
+        '''
+        Adiciona sequências por identificador.
+        '''
+        Id=identificador
+        sequence=seq
+        if self.valida(seq):
+            self.listaids.append(Id)
             if Id not in self.dic.keys():
-                self.dic[Id]={'Sequência':sequen}
+                self.dic[Id]={'Sequência':sequence}
                 self.dic[Id]['Anotações']={}
             else:
-                print('Identificador já utilizado')
+                print('Identificador já utilizado! Tente outro.')
         else:
-            print('Sequência proteica inválida')
+            print('Sequência de proteínas inválida!')
             
-    def addAnnotation(self,tipo,anno,Id):
+    def add_anotacoes(self,tipo,anno,Id):
         if tipo not in self.dic[Id]['Anotações'].keys():
             self.dic[Id]['Anotações'][tipo]=[]
             self.dic[Id]['Anotações'][tipo].append(anno)
         else:
             self.dic[Id]['Anotações'][tipo].append(anno)
             
-    def valSeq(self,seq):
+    def valida(self,seq):
         vs=MySeq(seq,'dna')
         if vs.validaER():
             return True
@@ -61,7 +66,7 @@ class GestorAA:
         for key,val in self.dic[Id].items():
                 print (key+':')
                 if type(val)==dict:
-                    self.printSubDic(val)
+                    self.printsubdicionario(val)
                 elif type(val)==list:
                     i=0
                     for x in range(len(val)):
@@ -71,20 +76,20 @@ class GestorAA:
                     print(self.dic[Id][key])
                     print()     
 
-    def printDic(self,dic=None):
+    def dicionario(self,dic=None):
         if dic==None:
             dic=self.dic
         else:
             dic=dic
-        l=self.listaIds
+        lista=self.listaIds
         tid=None
-        for x in range(len(l)):
-            tid=l[x]
+        for x in range(len(lista)):
+            tid=lista[x]
             print('>'+tid)
             for key,val in dic[tid].items():
                 print (key+':')
                 if type(val)==dict:
-                    self.printSubDic(val)
+                    self.subdicionario(val)
                 elif type(val)==list:
                     i=0
                     for x in range(len(val)):
@@ -94,11 +99,11 @@ class GestorAA:
                     print(dic[tid][key])
                     print()
                     
-    def __printSubDic(self,subdic):
+    def subdicionario(self,subdic):
         for key, val in subdic.items():
             print(key+':')
             if type(val)==dict:
-                self.printSubDic(val)
+                self.subdicionario(val)
             elif type(val)==list:
                 i=0
                 for x in range(len(val)):
@@ -108,19 +113,27 @@ class GestorAA:
                 print(subdic[key][val])
                 print()
         
-    def listIds(self):
+    def list_id(self):
         for key in self.dic.keys():
-            self.listaIds.append(key)
+            self.list_id.append(key)
                 
-    def editAnnotations(self,Id,tipo,nr,new):
+    def edit_anotacoes(self,Id,tipo,nr,new):
         self.dic[Id]['Anotações'][tipo][nr]=new
         
-    def editSeq(self,Id,new):
+    def edit_sequencias(self,Id,new):
         self.dic[Id]['Sequência']=new
         
-    def search_seqs(self,seq1):
+    def multialign(self,filename):
+        alignments = AlignIO.parse(filename, "fasta")
+        for alignment in alignments:
+            print ("Alinhamento:", alignment)
+        alignments = list(AlignIO.parse (filename, "fasta"))
+        last_align = alignments[-1]
+        first_align = alignments[0]
+        
+    def procura_seqs(self,seq1):
         db1=[]
-        for Id in self.listaIds:
+        for Id in self.listaids:
             db1.append(self.dic[Id]['Sequência'])
         blast=MyBlast(db=db1)
         query=seq1
@@ -131,43 +144,49 @@ class GestorAA:
             if self.dic[Id]['Sequência']==bestmatch:
                 print('Melhor match encontrado na sequência com Id %s' %Id)
     
-    def search_pattern(self,pat,Id):
+    def procura_pad(self,pad,Id):
         '''
-        Procura ocorrências de padrões definidos pelo utilizador nas sequências da base de dados.
+        Procura ocorrências de padrões definidos pelo utilizador nas sequências por identificador.
         '''
         seq=self.dic[Id]['Sequência']
-        matches = re.finditer(pat,seq)
+        matches = re.finditer(pad,seq)
         res=[]
         for match in matches:
             res.append(match.span())
         if len(res)!=0:
             print('Foram encontradas %d ocorrências do padrão na sequência %s' % (len(res),Id))
             i=1
-            for match in res:
-                print('%d ª ocorrência, posição: '%i,match)
+            for x in res:
+                print('%d ª ocorrência, posição: '%i,x)
                 i+=1
         else:
             print('Não foram encontradas ocorrências do padrâo na sequência %s' %Id)
             return None
 
-    def search_db(self,pat):
-        for Id in self.listaIds:
-            self.search_pattern(pat,Id)
+    def search_db(self,pad):
+        '''
+        Procura ocorrências de padrões de sequências numa base de dados.
+        '''
+        for Id in self.listaids:
+            self.search_pattern(pad,Id)
 
-    def freq(self,sim=None,Id=None):
+    def freq_simbolos(self,s=None,Id=None):
+        '''
+        Determina a frequência de símbolos de uma sequência selecionada.
+        '''
         if Id != None:
             seq=self.dic[Id]['Sequência']
             res=[]
-            matches=re.finditer(sim,seq)
+            matches=re.finditer(s,seq)
             for match in matches:
                 res.append(match.span())
             freq=len(res)/len(seq)
             print('O simbolo apresenta uma frequência de %f'%freq)
         else:
-            for Id in self.listaIds:
+            for Id in self.listaids:
                 seq=self.dic[Id]['Sequência']
                 res=[]
-                matches=re.finditer(sim,seq)
+                matches=re.finditer(s,seq)
                 for match in matches:
                     res.append(match.span())
                 freq=len(res)/len(seq)
@@ -179,7 +198,7 @@ class GestorAA:
         Imprime uma árvore filogenética das diferentes sequências de DNA com as distâncias entre as folhas e nodos.
         '''
         bd=[]
-        for Id in self.listaIds:
+        for Id in self.listaids:
             bd.append(MySeq(self.dic[Id]['Sequência'],'dna'))
         sm = SubstMatrix()
         sm.createFromMatchPars(3, -1, self.seqs_alfabeto)
@@ -273,15 +292,15 @@ class GestorAA:
             self.addSeq(ind,str(seq_record.seq))
             handle.close()
     
-    def make_seqs(self,filename,Ids):
+    def write_seqs(self,filename,ids):
         '''
         Permite a escrita de sequências num ficheiro.                                                                               
         '''
         try:
             file=open(cwd+'AlinMuExe\\'+filename+'.fasta','w')
-            for id_ in Ids:
-                file.write('>'+id_+'\n')
-                file.write(self.dic[id_]['Sequência']+'\n')
+            for Id in ids:
+                file.write('>'+Id+'\n')
+                file.write(self.dic[Id]['Sequência']+'\n')
             file.close()
         except:
             os.mkdir(cwd+'AlinMuExe\\')
@@ -303,5 +322,18 @@ class GestorAA:
         tree = Phylo.read(infile, "newick")
         Phylo.draw_ascii(tree)
         return tree
+
+    def RemindSeq(self):
+        for Id in self.listaids:
+            print(Id + '->' + self.dic[Id]["Sequência"])
     
-    
+    def import_(self,filename):
+        fasta_seq=SeqIO.parse(open(filename),"fasta")
+        for seq in fasta_seq:
+            if self.adicionar(seq.id,str(seq.seq).upper()):
+                self.add_anotacoes("nome",seq.nome,seq.id)
+                
+    def guardar_dic(self):
+        file=open(self.name+'.txt','wb')
+        pickle.dump(self.dic,file)
+        file.close()
--- Shell.py
+++ Shell.py
@@ -1,87 +1,208 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  1 17:53:13 2020
+
+@author: Francisco
+"""
 from MySeq import MySeq
+import pickle
 from cmd import *
-import re
-from Menu import GestorAA
-from ABC import Seq
+import sys
+from Menu import GestorSeq
 
-class GestorAAShell(Cmd):
-    intro = '''Interpretador de comandos para o gestor de sequências proteicas.\n
-    1. Criar sequências: inserir criar
-    2. Adicionar sequências: inserir addSeq
-    3. Procurar por identificador: inserir searchid
-    4.
-    5.
-    6. 
-    7. Sair do programa: inserir sair
-    '''
-    def do_criar(self,arg):
-        try:
-            nome=arg
-            if re.match('^[0-9]*$',nome):
-                eng.createNew(nome)
-        except:
-            print('Erro ao criar')
+class Shell(Cmd):
+    introd= """ Bem vindo ao gestor de sequências de DNA. Para iniciar introduzir "entrar". """
+    prompt= "Gestor de sequências -> "
     
-    def do_addSeq(self,arg):
-        try:
-            seq= input("Seq:")
-            a.valida(seq)
-
-        except:
-            print('Erro ao adicionar sequência')
-
-    def do_searchid(self,arg):
-        try:
-            if arg in eng.listIds():
-                eng.printDic()
+    def do_menu(self, arg=None):
+        while True:
+            print("""
+            ======== Teste das funções trabalho algoritmos ============
+            1. Criar uma base de dados
+            2. Importar uma base de dados
+            3. Adicionar sequências manualmente
+            4. Adicionar anotações
+            5. Editar sequências
+            6. Imprimir validade da sequência
+            7. Escrever sequências num ficheiro
+            8. Procurar a sequência mais semelhante na base de dados
+            9. Procurar um padrão
+            10. Determinar frequência de símbolos
+            11. Procurar padrões em bases de dados
+            12. Imprimir uma árvore filogenética
+            13. Fazer a tradução da sequência
+            14. Imprimir as ORFs
+            15. Fazer alinhamento múltiplo
+            16. Sair
+            """)
+            op = input("Escolha um teste: ")
+            if op == "1":
+                self.chamaCriar()
+            elif op == "2":
+                self.chamaFasta()
+            elif op == "3":
+                self.chamaAdicionar()
+            elif op == "4":
+                self.chamaAnotacoes()
+            elif op == "5":
+                self.chamaEditar()
+            elif op == "6":
+                self.chamaValida()
+            elif op == "7":
+                self.chamaEscritaSeqs()
+            elif op == "8":
+                self.chamaProcuraSeqs()
+            elif op == "9":
+                self.chamaProcuraPad()
+            elif op == "10":
+                self.chamaFreqSimb()
+            elif op == "11":
+                self.chamaSearchDB() 
+            elif op == "12":
+                self.chamaFilogenia()
+            elif op == "13":
+                self.chamaTraducao()
+            elif op == "14":
+                self.chamaOrfs()
+            elif op == "15":
+                self.chamaMultiAlign()
+            elif op == "16":
+                self.sair_do_menu()
+                break
             else:
-                print('Identificador não encontrado no gestor')
-        except:
-            print('Erro ao procurar por identificador')
-            
-    def do_editar(self,arg):
-        pass
+                print("\n Opção não valida. Tente de novo!")
+        print("\n Obrigado por ter usado o gestor de sequências!") 
     
-    def do_print(self,arg):
-        eng.printDic()
-        
-    def do_getSeqsOnline(self, arg):
-        pass
-        
-    def do_sair(self, arg):
-        "Sair do programa"
-        print('Obrigado por ter utilizado o gestor de sequências!')
+    def chamaCriar(self):
+        try:
+            nome=input("insira o nome da base de dados ")
+            gest.criar(nome)
+        except:
+            print("Erro ao dar o nome à base de dados!")
+    
+    def chamaFasta(self):
+        try:
+            filename=input("Escreva a base de dados a importar ")
+            gest._import(filename)
+        except:
+            print("Erro ao importar!")
+            
+    def chamaAdicionar(self):
+        try:
+            Id=input("Insira um identificador ")
+            seq=input("Introduza uma sequência ")
+            gest.adicionar(Id,seq)
+        except:
+            print("Erro ao adicionar a sequência!")
+    
+    def chamaAnotacoes(self):
+        try:
+            Id=input("Insira um identificador da sequência ")
+            anotacao=input("Introduza a anotação ")
+            tipo=input("Introduza um tipo de anotação ")
+            gest.add_anotacoes(tipo,anotacao,Id)
+        except:
+            print("Erro ao adicionar anotação!")
+    
+    def chamaEditar(self):
+        try:
+            Id=input("Insira um identificador da sequência ")
+            new=input("Nova sequência:")
+            gest.edit_sequencias(Id,new)
+        except:
+            print("Erro ao editar sequências!")
+    
+    def chamaValida(self):
+        try:
+            seq=input("Escreva uma sequência ")
+            gest.valida(seq)
+        except:
+            print("Erro ao validar a sequência!")
+    
+    def chamaEscritaSeqs(self):
+        try:
+            filename=input("Ficheiro ")
+            ids=input("Identificador ")
+            gest.write_seqs(filename,ids)
+        except:
+            print("Erro ao escrever!")
+    
+    def chamaProcuraSeqs(self):
+        try:
+            seq1=input("Escreva a sequência a procurar ")
+            gest.procura_seqs(seq1)
+        except:
+            print("Erro ao procurar!")
+    
+    def chamaProcuraPad(self):
+        try:
+            Id=input("Escreva um identificador ")
+            pad=input("Introduza um padrão que pretenda procurar ")
+            gest.procura_pad(pad,Id)
+        except:
+            print("Erro ao procurar!")
+    
+    def chamaFreqSimb(self):
+        try:
+            s=input("Escreva o símbolo ")
+            Id=input("Introduza o identificador ")
+            gest.freq_simbolos(s,Id)
+        except:
+            print("Erro ao procurar!")
+    
+    def chamaSearchDB(self):
+        try:
+            pad=input("Introduza um padrão ")
+            gest.search_db(pad)
+        except:
+            print("Erro ao procurar!")
+    
+    def chamaFilogenia(self):
+        try:
+            gest.arvore_filogenetica()
+        except:
+            print("Erro ao criar árvore filogenética!")
+    
+    def chamaMultiAlign(self):
+        try:
+            file=input("Nome do ficheiro ")
+            gest.multialign(file)
+        except:
+            print("Erro ao fazer alinhamento múltiplo!")
+    
+    def chamaTraducao(self):
+        try:
+            Id=input("Escreva um identificador da sequência ")
+            gest.traducao(Id)
+        except:
+            print("Erro ao efetuar a tradução!")
+    
+    def chamaOrfs(self):
+        try:
+            Id=input("Introduza um identificador da sequência ")
+            gest.orfs(Id)
+        except:
+            print("Erro!")
+    
+    def sair_do_menu(self):
+        "Sair do menu"
+        print("Saída do menu feita com sucesso!")
+        global janela
+        if janela is not None:
+            del janela
+        return True
+    
+    def do_sair(self,arg):
+        "Sair do gestor de sequências"
+        print("Obrigado por usar o gestor de sequências!")
         global janela
         if janela is not None:
             del janela
         return True
 
-def adicionar():
-    seq_dna = input("Sequencia:")
-    s1 = MySeq(seq_dna)
-    s1.printseq()
-    
-def teste_ficheiro():
-    ficheiro = input("Nome ficheiro: ")
-    #por mensagem de erro caso o ficheiro não existir
-    
-    with open(f"{ficheiro}.txt", 'r') as readf:
-        readf.readline()
-        seq_ficheiro = readf.read()
-        seq_ficheiro2 = seq_ficheiro.replace(" ", "")
-        seq_ficheiro3 = seq_ficheiro2.replace("\n", "")
-    
-    s2 = MySeq(seq_ficheiro3)
-    db.adicionar(s2)
-    s2.guardar(seq_ficheiro3)
-    s2.get_guardar()
-    s2.printseq()
-    print(s2.valida())
-
-if __name__ == '__main__':
-    eng = GestorAA()
-    a=Seq()
-    janela = None
-    sh = GestorAAShell()
+            
+if __name__ == "__main__":
+    gest = GestorSeq()
+    janela=None
+    sh= Shell()
     sh.cmdloop()
-    
